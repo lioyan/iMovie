8b767f56efcfe58acc714d89b159efabd2d37bcf
lioyan <409610613@qq.com>
2016-12-14 21:18:34 +0800

    add 后台逻辑

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var path = require('path');
+var mongoose = require('mongoose');
+var Movie = require('./models/movie');
+var _ = require('underscore');
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
 var app = express();
+
+mongoose.connect('mongodb://localhost/iMovie');
 
 app.set('views','./views/pages');
 app.set('view engine','jade');
 //app.use([path],function) 使用中间件
-//app.use(express.bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'bower_components')));
+app.locals.moment = require('moment');
 app.listen(port, function(){
 	console.log('iMovie is started on port ' + port);
 });
@@ -15,57 +23,28 @@ app.listen(port, function(){
 //添加路由
 //index page
 app.get('/', function(req,res){
-	res.render('index', {
-		title : 'iMovie 首页',
-		movies: [
-			{
-				title: '变形金刚',
-				_id: 1,
-				poster: 'http://r4.ykimg.com/05160000564ED6E067BC3C28CA0335FC'
-			},
-			{
-				title: '变形金刚',
-				_id: 2,
-				poster: 'http://r4.ykimg.com/05160000564ED6E067BC3C28CA0335FC'
-			},
-			{
-				title: '变形金刚',
-				_id: 3,
-				poster: 'http://r4.ykimg.com/05160000564ED6E067BC3C28CA0335FC'
-			},
-			{
-				title: '变形金刚',
-				_id: 4,
-				poster: 'http://r4.ykimg.com/05160000564ED6E067BC3C28CA0335FC'
-			},
-			{
-				title: '变形金刚',
-				_id: 5,
-				poster: 'http://r4.ykimg.com/05160000564ED6E067BC3C28CA0335FC'
-			},
-			{
-				title: '变形金刚',
-				_id: 6,
-				poster: 'http://r4.ykimg.com/05160000564ED6E067BC3C28CA0335FC'
-			}
-		]
+	Movie.fetch(function(err,movies){
+		if(err){
+			console.log(err)
+		}
+		res.render('index', {
+			title : 'iMovie 首页',
+			movies: movies
+		})
 	})
 });
 
 //detail page
 app.get('/movie/:id', function(req,res){
-	res.render('detail', {
-		title : 'iMovie 详情页',
-		movie: {
-			director: '迈克尔·贝',
-			country: '美国',
-			title: '变形金刚',
-			year: 2007,
-			poster: 'http://r4.ykimg.com/05160000564ED6E067BC3C28CA0335FC',
-			language: '英语',
-			flash: '//static.youku.com/v20161213.0/v/swf/loader.swf',
-			summary: '简介：“霸天虎”的先遣部队旋风和毒蝎袭击了美军位于卡塔尔的军事基地，与此同时，路障帮助他的搭档迷乱潜入了美国总统的座机空中一号，通过电脑获悉，要想找到威震天就必须找到维特维奇家族的那副眼镜，上面有威震天关于能量块的信息扫描，而它现在的拥有者萨姆·维特维奇（希安·拉博夫 饰）成为了“霸天虎”攻击的目标。萨姆是名高中生，学校里没有人相信他讲述的关于这副眼镜的历史，失望之余，萨姆把眼镜放到网上拍卖，然而从未引起买家的兴趣。这时父亲送了一辆破车给他，这辆车恰好就是“汽车人”大黄蜂的变形。大黄蜂帮助萨姆交上了漂亮的女朋友米凯拉（梅根·福克斯 饰），但很快就遭遇了“霸天虎”的袭击，擎天柱带着其他“汽车人”赶到，一场机器人大战由此拉开了序幕。'
+	var id = req.params.id;
+	Movie.findById(id,function(err,movie){
+		if(err){
+			console.log(err);
 		}
+		res.render('detail', {
+			title : 'iMovie ' + movie.title,
+			movie: movie
+		})
 	})
 });
 
@@ -86,20 +65,60 @@ app.get('/admin/movie', function(req,res){
 	})
 });
 
+//admin update movie
+app.get('/admin/update/:id', function(req,res){
+	var id = req.params.id;
+	Movie.findById(id, function(err,movie){
+		if(err) console.log(err);
+		res.render('admin', {
+			title: 'iMovie 后台更新页',
+			movie: movie
+		})
+	})
+})
+
+//admin post movie
+app.post('/admin/movie/new', function(req,res){
+	var movieObj = req.body.movie;
+	var id = movieObj._id;
+	var _movie;
+
+	if(id !== 'undefined'){
+		Movie.findById(id,function(err,movie){
+			if(err) console.log(err);
+			_movie = _.extend(movie,movieObj);
+			_movie.save(function(err,movie){
+				if(err) console.log(err);
+				res.redirect('/movie/' + movie._id);
+			})
+		})
+	}
+	else{
+		_movie = new Movie({
+			director: movieObj.director,
+			title: movieObj.title,
+			country: movieObj.country,
+			language: movieObj.language,
+			year: movieObj.year,
+			summary: movieObj.summary,
+			flash: movieObj.flash,
+			poster: movieObj.poster,
+		});
+		_movie.save(function(err,movie){
+			if(err) console.log(err);
+			res.redirect('/movie/' + movie._id);
+		})
+	}
+
+})
+
 //list page
 app.get('/admin/list', function(req,res){
-	res.render('list', {
-		title : 'iMovie 列表页',
-		movies: [
-			{
-				_id: 1,
-				director: '迈克尔·贝',
-				country: '美国',
-				title: '变形金刚',
-				year: 2007,
-				poster: 'http://r4.ykimg.com/05160000564ED6E067BC3C28CA0335FC',
-				language: '英语',
-			}
-		]
+	Movie.fetch(function(err,movies){
+		if(err) console.log(err);
+		res.render('list', {
+			title : 'iMovie 列表页',
+			movies: movies
+		})
 	})
 });
---
+++ models/movie.js
@@ -0,0 +1,5 @@
+var mongoose = require('mongoose');
+var movieSchema = require('../schemas/movie');
+var Movie = mongoose.model('Movie',movieSchema);
+
+module.exports = Movie;
